4f209ad859ee8cf34c630ca8a74b27b2d409433b
Abbath <pitongogi@gmail.com>
2016-01-26 14:44:16 +0100

    Fix Manager

--- src/Ivona.hs
+++ src/Ivona.hs
@@ -7,7 +7,6 @@ import Data.Text (Text)
 import Data.ByteString.Lazy
 import Ivona.Types
 import qualified Network.HTTP.Client as HTTPClient
-import qualified Network.HTTP.Client.TLS as HTTPClientTLS
 import Network.Wreq
 
 ivonaAPI :: String
@@ -19,9 +18,8 @@ createSpeechAPI = ivonaAPI ++ "/CreateSpeech"
 listVoicesAPI :: String
 listVoicesAPI = ivonaAPI ++ "/ListVoices"
 
-createSpeech :: Text -> IO ByteString 
-createSpeech s = do
-  mgr <- HTTPClient.newManager HTTPClientTLS.tlsManagerSettings
+createSpeech :: Text -> HTTPClient.Manager -> IO ByteString 
+createSpeech s mgr = do
   let opts = defaults & manager .~ (Right mgr)
                       & auth ?~ awsAuth AWSv4 "GDNAI7ROPRZLGYVRAJYQ" "x4yaAVE5dWl7+sU2kkxAgpGsd68/8kdDYHNekoHj" 
                       & header "Content-Type" .~ ["application/json"]
--- src/Main.hs
+++ src/Main.hs
@@ -8,6 +8,8 @@ import qualified Data.Text.IO as TIO
 import           Formatting
 import           Ivona
 import           Options.Applicative
+import qualified Network.HTTP.Client as HTTPClient
+import qualified Network.HTTP.Client.TLS as HTTPClientTLS
 
 data Options = Options { 
       ifile :: String,
@@ -32,15 +34,16 @@ main = do
   Options infile outfile <- execParser opts
   content <- TIO.readFile infile
   let contents = T.splitOn "\n\n" content
+  mgr <- HTTPClient.newManager HTTPClientTLS.tlsManagerSettings
   forM_ (zip contents [(1::Int)..]) $ \(x, n) -> do
-         TIO.putStrLn $ sformat ("Paragraph number " % int % " is being processed.") n 
-         speech <- createSpeech x
-         let newfilename = outfile ++ T.unpack (sformat ((left 4 '0' %. int) % ".mp3") n)
-         B.writeFile newfilename speech
-         TIO.putStrLn $ sformat ("File " % string % " has been created.") newfilename 
+    TIO.putStrLn $ sformat ("Paragraph number " % int % " is being processed.") n 
+    speech <- createSpeech x mgr
+    let newfilename = outfile ++ T.unpack (sformat ((left 4 '0' %. int) % ".mp3") n)
+    B.writeFile newfilename speech
+    TIO.putStrLn $ sformat ("File " % string % " has been created.") newfilename 
   where opts = info (helper <*> options)
-               ( fullDesc
-                 <> progDesc "Read INPUTFILE and convert each paragraph to OUTPUTFILE<number>.mp3"
-                 <> header "Ivona - program for audiobook from text creation" )
+           ( fullDesc
+           <> progDesc "Read INPUTFILE and convert each paragraph to OUTPUTFILE<number>.mp3"
+           <> header "Ivona - program for audiobook from text creation" )
              
   
